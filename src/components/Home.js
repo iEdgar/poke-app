14d7a8b166b6e2f1a069d3bae24bd1ebefa35f33
Edgar Reyes <54544372+iLata@users.noreply.github.com>
2019-09-11 10:33:33 -0500

    inicio del proyecto

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../App.css';
+
+const Home = () => {
+    return(
+        <div className="App">
+            <header className="App-header">
+                <img src={process.env.PUBLIC_URL + '/images/poke.png'} width='400' alt='poke'/>
+                <h1>Poke App</h1>
+                <button>Ver pókemons</button>
+            </header>
+        </div>
+    );
+}
+
+export default Home;
